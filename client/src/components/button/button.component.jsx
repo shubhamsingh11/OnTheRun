1f5c970c6dbd7ef810b9c3fa410e8ef4009d1740
shubhamsingh11 <shubham11singh1997@gmail.com>
2021-06-02 04:16:32 +0530

    Refactored code and created button's own component

---
+++ client/src/components/button/button.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './button.styles.css';
+
+const Button = () => {
+    return(
+        <button className="btn" >Check Story</button>
+)}
+
+export default Button;
--- client/src/pages/Posts/post.component.jsx
+++ client/src/pages/Posts/post.component.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './post.styles.css';
 import Location from '../../components/Location/location.component';
+import Button from "../../components/button/button.component";
 const Post = () => {
     return(
     <div className="post">
         <Location/>
-        <button className="btn" >Check Story</button>
+        <Button/>
     </div>
 )}
 
